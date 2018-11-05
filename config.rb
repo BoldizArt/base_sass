2f9e84f79c8af3b569b0a1740a1480077290ba86
BoldizArt <boldizar.santo@gmail.com>
2018-11-05 18:56:38 +0100

    Base files and config.rb file.

---
+++ config.rb
@@ -0,0 +1,11 @@
+# Tell compass to use scss format
+preferred_syntax = :scss
+
+# Define css directory
+css_dir = 'css'
+
+# Define sass source directory
+sass_dir = 'sass'
+
+# Debug the css styles id dev mode
+line_comments = true
